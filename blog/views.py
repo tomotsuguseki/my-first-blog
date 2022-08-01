61c69cff3604eacec134024a2da11ff16cd73d55
tomotsuguseki <tomotsuguseki@gmail.com>
2022-08-01 17:37:12 +0900

    Revised some error.

--- blog/views.py
+++ blog/views.py
@@ -13,4 +13,4 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, "blog/post_detai.html", {"post": post})
+    return render(request, "blog/post_detail.html", {"post": post})
